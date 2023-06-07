b700112df08c01c88ccf2ffe293f9ec1cc53c2db
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-06-07 13:55:33 -0500

    update: add new types and resolver for role & permission

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,14 +2,16 @@ import gql from 'graphql-tag';
 import { UsertypeDefs, UserResolvers } from './user.schema';
 import { AuthTypeDefs, AuthResolvers } from './auth/auth.schema';
 import { BlogtypeDefs, BlogResolvers } from './blog.schema';
+import { RolesTypeDefs, RolResolvers } from './Roles.schema';
+import { PermissionResolvers, PermissionTypeDefs } from './permission.schema';
 const rootTypeDefs = gql`
   type Query {
     _: String
   }
 
   type messageCrud {
-    succes: Boolean!
-    message: String!
+    succes: Boolean
+    message: String
   }
 
   type Mutation {
@@ -21,6 +23,14 @@ export const typeDefs = [
   rootTypeDefs,
   AuthTypeDefs,
   UsertypeDefs,
-  BlogtypeDefs
+  BlogtypeDefs,
+  RolesTypeDefs,
+  PermissionTypeDefs
 ];
-export const resolvers = [AuthResolvers, UserResolvers, BlogResolvers];
+export const resolvers = [
+  AuthResolvers,
+  UserResolvers,
+  BlogResolvers,
+  RolResolvers,
+  PermissionResolvers
+];
