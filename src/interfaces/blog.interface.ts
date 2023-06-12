f8ff7cbf083950e9eb8f03c73ba228a89be99211
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-06-11 23:27:48 -0500

    updated: correcting values ​​and adding fields origin && status

--- src/interfaces/blog.interface.ts
+++ src/interfaces/blog.interface.ts
@@ -1,13 +1,14 @@
 import mongoose, { Document, Model } from 'mongoose';
-import { IUser } from './user.interface';
 
 export interface IBlog {
   title: String;
-  body_content: string;
-  front_image: String;
+  body_content: String;
+  front_image: mongoose.Types.ObjectId;
   slug_title: String;
   count_view: Number;
   author: mongoose.Types.ObjectId;
+  status: Boolean;
+  origin: String;
 }
 
 export interface IBlogDocument extends IBlog, Document {}
---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export * from './blog.interface';
+export * from './image.interface';
+export * from './permission.interface';
+export * from './rol.interface';
+export * from './user.interface';
