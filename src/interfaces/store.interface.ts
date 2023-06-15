2adcdf56aed487a1b077b8a05628895a57227bbb
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-06-15 17:28:50 -0500

    ADD: model interface store

---
+++ src/interfaces/store.interface.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document } from 'mongoose';
+import { IImage } from './image.interface';
+import { ITag } from './tag.interfaces';
+import { ICategory } from './category.interface';
+
+export interface IStore {
+  title: string;
+  sub_title: string;
+  slug: string;
+  description: string;
+  main_image: mongoose.Types.ObjectId; //ref 'img'
+  logo?: mongoose.Types.ObjectId; //ref 'img'
+  galleries_image?: mongoose.Types.DocumentArray<IImage>; //ref 'img'
+  phone: string;
+  address: string;
+  positionX: string;
+  positionY: string;
+  region: string;
+  country?: string;
+  url_video?: string;
+  url_website?: string;
+  email: string;
+  socials?: Social[];
+  times_tables: Times[];
+  onwer: mongoose.Types.ObjectId; //ref 'user'
+  tags?: mongoose.Types.DocumentArray<ITag>; //ref 'tags'
+  categories?: mongoose.Types.DocumentArray<ICategory>;
+}
+
+type Times = {
+  week_name: string;
+  open_time: string;
+  close_time: string;
+  open: boolean;
+};
+
+type Social = {
+  name_social: string;
+  url: string;
+};
+
+export interface IStoreDocument extends IStore, Document {}
