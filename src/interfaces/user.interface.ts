bcd496c026b63e9ce8814210f7d22e2344ab1af8
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-06-15 17:27:02 -0500

    ADD: and new fields branchs & store relations

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,17 +1,22 @@
 import mongoose, { Document, Model } from 'mongoose';
+import { IBranch } from './branch.interface';
+import { IStore } from './store.interface';
 
 export interface IUser {
-  firstname: String;
-  lastname: String;
-  email: String;
-  phone: String;
-  website?: String;
-  password: String;
-  rol: mongoose.Types.ObjectId;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  website?: string;
+  password: string;
+  rol?: mongoose.Types.ObjectId;
+  branchs?: mongoose.Types.DocumentArray<IBranch>;
+  stores?: mongoose.Types.DocumentArray<IStore>;
 }
-
+//methods
 export interface IUserDocument extends IUser, Document {}
 
+//static
 export interface IUserModel extends Model<IUserDocument> {
   encryptPassword(password: String): String;
   comparePassword(password: String, recivePassword: String): Boolean;
