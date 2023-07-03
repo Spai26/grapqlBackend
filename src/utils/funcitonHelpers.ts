71efd1ed9ca64f5e9f67f3f19864e166ad8a0f6e
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-07-03 17:37:54 -0500

    add function generate slug

--- src/utils/funcitonHelpers.ts
+++ src/utils/funcitonHelpers.ts
@@ -29,3 +29,10 @@ export const fileFilter = (
 export const removeExtends = (filename: string) => {
   return filename.split('.').shift();
 };
+
+export const generateSlug = (data: string) => {
+  return data
+    .toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-');
+};
